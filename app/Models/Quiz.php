f4e9f9ac3ae5c8890140951670b814863cf50943
HabibaMedhat <82908854+HabibaMedhat@users.noreply.github.com>
2024-09-27 02:55:50 +0300

    Add files via upload

---
+++ app/Models/Quiz.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Quiz extends Model
+{
+    use HasFactory;
+
+    public function userAnswerQuiz()
+    {
+        return $this->hasMany(User_Answer::class);
+    }
+
+    protected $fillable = ['title', 'time_limit'];
+
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
+
+
+
+    // Define the relationship with Topic
+    public function topic()
+    {
+        return $this->belongsTo(Topic::class);
+    }
+}
---
+++ app/Models/Topic.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Topic extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name'];
+
+    // Define the relationship with Quiz
+    public function quizzes()
+    {
+        return $this->hasMany(Quiz::class);
+    }
+
+
+}
