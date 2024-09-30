bd1cf2ddcef57e42936fc2cf9d5abb365f21d78b
Marioo-Ayman <96667299+Marioo-Ayman@users.noreply.github.com>
2024-09-30 17:20:31 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,1 @@
-<?php
 
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\QuizController;
-use Illuminate\Support\Facades\Route;
-
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::get('/quiz/select', [QuizController::class, 'selectForm'])->name('quiz.selectForm');
-Route::post('/quiz/setup', [QuizController::class, 'setupQuiz'])->name('quiz.setup');
-
-Route::get('/quiz/create', [QuizController::class, 'createQuizForm'])->name('quiz.createForm');
-Route::post('/quiz/store', [QuizController::class, 'store'])->name('quiz.store');
